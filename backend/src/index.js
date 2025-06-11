3a80b6de383b51e256c0579a96b85f167ecb664f
LexieC <dkt4kr@virginia.edu>
2025-06-11 15:58:15 -0700

    Fix duplicate path import

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,6 @@ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
-import path from "path"; 
 
 dotenv.config();
 
