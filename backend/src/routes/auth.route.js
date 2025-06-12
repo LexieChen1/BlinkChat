6792c1bc2d165f6abbbf1f38bce59ee41c831590
LexieC <dkt4kr@virginia.edu>
2025-06-12 12:53:07 -0700

    Fix update-profile route with multer and auth

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -7,25 +7,19 @@ const router = express.Router();
 const storage = multer.memoryStorage(); 
 const upload = multer({ storage, limits: { fileSize: 5 * 1024 * 1024 } }); // 5MB max
 
-// Upload route
-router.put("/update-profile", upload.single("profilePic"), async (req, res) => {
-  const file = req.file;
-  if (!file) return res.status(400).send("No file uploaded");
-
-  const base64 = `data:${file.mimetype};base64,${file.buffer.toString("base64")}`;
-
-  res.status(200).json({ message: "Profile updated", image: base64 });
-});
-
-
 router.post("/signup", signup);
 
 router.post("/login", login);
 
 router.post("/logout", logout);
 
-//check if they're authenticated 
-router.put("/update-profile", protectRoute, updateProfile); 
+router.put(
+  "/update-profile",
+  protectRoute,
+  upload.single("profilePic"),
+  updateProfile
+);
+
 
 router.get("/check", protectRoute, checkAuth); 
 
